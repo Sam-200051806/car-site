6e0c471f82f6ecb5bdd7a503b555cf310e0812c8
Sambhav seth <sambhavseth1806@gmail.com>
2024-10-27 18:37:50 +0530

    added cars app and fetch the data from the database to the home screen

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Car
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id','car_title','city','color','year','body_style','fuel_type','is_featured')
+    list_display_links = ('id','car_title')
+    list_editable = ('is_featured',)
+    search_fields = ('id','car_title','fuel_type','is_featured')
+    list_filter = ('city' ,'body_style' ,'model','fuel_type')
+admin.site.register(Car,CarAdmin)
