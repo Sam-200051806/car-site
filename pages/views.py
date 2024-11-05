06cf4d455ea77c52d36bb9ff827b105246d2c396
Sambhav seth <sambhavseth1806@gmail.com>
2024-11-06 02:07:04 +0530

    search and paginator

--- pages/views.py
+++ pages/views.py
@@ -6,10 +6,19 @@ def home(request):
     teams = Team.objects.all()
     featured_car = Car.objects.order_by('-created_date').filter(is_featured = True)
     all_cars = Car.objects.order_by("-created_date")
+    # search_feilds = Car.objects.values('model','year','city','body_style')
+    model_feilds = Car.objects.values_list('model',flat=True).distinct()
+    city_feilds = Car.objects.values_list('city',flat=True).distinct()
+    year_feilds = Car.objects.values_list('year',flat=True).distinct()
+    body_style_feilds = Car.objects.values_list('body_style',flat=True).distinct()
     data = {
         'teams' : teams,
         'featured_car' : featured_car,
         'all_cars' : all_cars,
+        'city_feilds' : city_feilds,
+        'model_feilds' : model_feilds,
+        'year_feilds' : year_feilds,
+        'body_style_feilds' : body_style_feilds,
     }
     return render(request,'pages/home.html',data)
 
